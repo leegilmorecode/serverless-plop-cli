4305d8d9c8156f7dbe2511860b13ae2b3d4bb377
Lee Gilmore <lee.gilmore@gmail.com>
2024-05-11 11:24:15 +0100

    fixed breaking change of lambda powertools

--- advocate-cli/src/action-types/install-deps/install-deps.ts
+++ advocate-cli/src/action-types/install-deps/install-deps.ts
@@ -4,7 +4,7 @@ import { executeNpmCommand } from '../../utils/execute-npm-command.js';
 export const installDeps = (plop: NodePlopAPI) => {
   return plop.setActionType('install deps', () => {
     executeNpmCommand(
-      'npm i uuid @aws-lambda-powertools/metrics @aws-lambda-powertools/tracer @aws-lambda-powertools/logger @middy/core ajv ajv-formats && npm i -D @types/aws-lambda @types/uuid esbuild'
+      'npm i convict uuid @aws-lambda-powertools/metrics @aws-sdk/client-dynamodb @aws-sdk/client-sns @aws-sdk/util-dynamodb @middy/sqs-partial-batch-failure @aws-lambda-powertools/tracer @aws-lambda-powertools/logger @middy/core ajv ajv-formats && npm i -D @types/aws-lambda @types/uuid esbuild'
     );
     return 'dependencies installing...';
   });
